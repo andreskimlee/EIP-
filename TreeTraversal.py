f9c9e21797e048da59d38401afaa736de632e58a
Andres Kim <andreskimlee@gmail.com>
2020-05-06 15:27:44 -0400

    DS

--- TreeTraversal.py
+++ TreeTraversal.py
@@ -9,14 +9,3 @@ def printInorder(root):
 
         # now recur on right child
         printInorder(root.right)
-
-
-[0, 0], [0, 1], [0, 2]
-
-ships = 3
-
-[0, x, 0, 0, 0, 0, x]
-[0, x, 0, 0, 0, 0, x]
-[0, x, 0, 0, 0, 0, x]
-[0, 0, 0, 0, 0, 0, 0]
-[0, 0, 0, 0, 0, 0, 0]
